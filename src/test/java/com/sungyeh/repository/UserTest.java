2e34b6d26627dcaac7988b779203fad9a8538e8c
sungyeh <sungyeh@tist.com.tw>
2018-11-19 11:05:16 +0800

    junit

--- src/test/java/com/sungyeh/repository/UserTest.java
+++ src/test/java/com/sungyeh/repository/UserTest.java
@@ -1,16 +1,20 @@
 package com.sungyeh.repository;
 
+import com.sungyeh.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserTest {
+    @Resource
+    private UserService userService;
     @Test
     public void test(){
-        System.out.println("ttttt");
-        System.err.println("test");
+        System.out.println(userService.findAll());
     }
 }
